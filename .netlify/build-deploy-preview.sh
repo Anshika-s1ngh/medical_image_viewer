d4dda4ae1976a1138ced57a3280d9538859ca766
Anshika-s1ngh <135135105+Anshika-s1ngh@users.noreply.github.com>
2024-11-25 15:28:54 +0530

    Add files via upload

---
+++ .netlify/build-deploy-preview.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+cd .. # Up to project root
+
+# Helpful to verify which versions we're using
+echo 'My yarn version is... '
+
+yarn -v
+node -v
+
+# Build && Move PWA Output
+yarn run build:ci
+mkdir -p ./.netlify/www/pwa
+mv platform/app/dist/* .netlify/www/pwa -v
+echo 'Web application built and copied'
+
+# Build && Move Docusaurus Output (for the docs themselves)
+cd platform/docs
+yarn install
+yarn run build
+cd ../..
+mkdir -p ./.netlify/www/docs
+mv platform/docs/build/* .netlify/www/docs -v
+echo 'Docs built (docusaurus) and copied'
+
+# Cache all of the node_module dependencies in
+# extensions, modules, and platform packages
+yarn run lerna:cache
+echo 'Nothing left to see here. Go home, folks.'
